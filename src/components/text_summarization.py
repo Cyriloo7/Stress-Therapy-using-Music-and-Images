5c7d0f17a380ba726e68badfbadf78b290dc650e
cyril jose <cyril.jose2020@vitstudent.ac.in>
2024-07-22 22:08:34 +0530

    latest

--- src/components/text_summarization.py
+++ src/components/text_summarization.py
@@ -1,15 +1,37 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
 import torch
+import sys
+from src.logger.logger import logger
+from src.exceptions.exception import customexception
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-inputs = tokenizer(text, max_length=8192, return_tensors="pt")
+class TextSummarizer():
+     def __init__(self):
+          self.pipe = pipeline("summarization", model="Azma-AI/bart-large-text-summarizer")
+          self.pipe = self.pipe.to(device)
+     def first_summarize(self, text):
+          try:
+               logger.info("first summarization started")
+               summary = self.pipe(("summary of : "+text),max_length=512, min_length=150, do_sample=True, clean_up_tokenization_spaces=True)
+               logger.info(" first summarization finished")
+               return summary[0]['summary_text']
+          except Exception as e:
+               raise customexception(e, sys)
+          
 
-max_token_id = inputs['input_ids'].max().item()
-vocab_size = tokenizer.vocab_size
-if max_token_id >= vocab_size:
-     return jsonify({'error': f'Token ID {max_token_id} exceeds vocabulary size of {vocab_size}'})
-attention_mask = inputs.attention_mask
-inputs = inputs.to(device)
-attention_mask = attention_mask.to(device)
+     def second_summarize(self, text):
+          try:
+               logger.info("second summarization started")
+               summary = self.pipe(("summary of : "+text),max_length=200, min_length=20, do_sample=True, clean_up_tokenization_spaces=True)
+               logger.info(" second summarization finished")
+               return summary[0]['summary_text']
+          except Exception as e:
+               raise customexception(e, sys)
+          
+
+if __name__ == "__main__":
+     summarizer = TextSummarizer()
+     print(summarizer.first_summarize("This is a sample text to summarize."))
+     print(summarizer.second_summarize("This is a sample text to summarize."))
