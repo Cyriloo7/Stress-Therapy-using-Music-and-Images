e5957b0a76dfc8f911928015ce9f7d9430fbf259
cyril jose <cyril.jose2020@vitstudent.ac.in>
2024-07-22 22:08:22 +0530

    latest

--- src/components/text_summarization.py
+++ src/components/text_summarization.py
@@ -3,10 +3,6 @@ import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-tokenizer = AutoTokenizer.from_pretrained("Azma-AI/bart-large-text-summarizer")
-tokenizer.pad_token = tokenizer.eos_token
-model = AutoModelForSeq2SeqLM.from_pretrained("Azma-AI/bart-large-text-summarizer")
-model = model.to(device)
 
 inputs = tokenizer(text, max_length=8192, return_tensors="pt")
 
@@ -17,7 +13,3 @@ if max_token_id >= vocab_size:
 attention_mask = inputs.attention_mask
 inputs = inputs.to(device)
 attention_mask = attention_mask.to(device)
-
-# Generate Summary
-summary_ids = model.generate(inputs["input_ids"], min_length=150, max_length=512, attention_mask=attention_mask)
-decoded_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=False)
